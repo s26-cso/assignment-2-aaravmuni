d1087a21849a360e807db9252701477bac663ed3
aarav.muni <aarav.muni@gmail.com>
2026-04-14 23:11:42 +0530

    q4 done

---
+++ q4/q4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <dlfcn.h>
+#include <string.h>
+
+int main()
+{
+    char operations[100];
+    int a,b;
+
+    scanf("%s %d %d", operations, &a, &b);
+
+    char libname[100] = "lib";
+    strcat(libname, operations);
+    strcat(libname, ".so");
+
+    void *handle = dlopen(libname, RTLD_LAZY);
+
+    int(*func)(int,int);
+    func = dlsym(handle,operations);
+
+    int result = func(a,b);
+
+    printf("%d\n", result);
+
+    dlclose(handle);
+
+    return 0;
+}
